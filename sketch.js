4109963b2949cbbfcdc9f7935f64f6b904ac2d0f
shireen30 <70129608+shireen30@users.noreply.github.com>
2021-02-14 14:35:23 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -28,7 +28,7 @@ function preload(){
   obstacle5 = loadImage("obstacle5.png");
   obstacle6 = loadImage("obstacle6.png");
 vampireImg=loadImage("vampire.png");
-robotImg=loadImage("robot3.png");
+robotImg=loadImage("robot4.png");
   
   gameOverImg = loadImage("gameOver.png");
   restartImg = loadImage("restart.png");
